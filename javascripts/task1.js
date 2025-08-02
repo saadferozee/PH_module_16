9cfd5a2bbb3593521145de1d8215bc9e568307e0
saadferozee <saadferozee@gmail.com>
2025-08-03 02:23:48 +0600

    practice file added

---
+++ javascripts/task1.js
@@ -0,0 +1,13 @@
+const colors = {
+    red: '#FF0000',
+    green: '#00FF00',
+    blue: '#0000FF',
+    "golden rod": '#DAA520',
+};
+
+const gold = "golden rod"
+const getColor = colors[gold];
+
+console.log (getColor);
+
+// Output : #DAA520
